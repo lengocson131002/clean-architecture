e27e9c203da32041a27ad7036aea0c025335730c
sonln1 <sonln1@ocb.com.vn>
2024-01-03 22:17:25 +0700

    Fix spring response body

--- presentation-spring/src/main/java/vn/com/ocb/controller/UserController.java
+++ presentation-spring/src/main/java/vn/com/ocb/controller/UserController.java
@@ -3,6 +3,8 @@ package vn.com.ocb.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import vn.com.ocb.model.response.BaseResponse;
+import vn.com.ocb.model.response.BaseResponseBuilder;
 import vn.com.ocb.pipeline.request.RequestPipeline;
 import vn.com.ocb.usecase.auth.model.request.LoginRequest;
 import vn.com.ocb.usecase.user.model.request.CreateUserRequest;
@@ -23,38 +25,65 @@ public class UserController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    List<UserResponse> getAllUsers() {
+    BaseResponse<List<UserResponse>> getAllUsers() {
         GetAllUserRequest request = new GetAllUserRequest();
-        return requestPipeline
-                .send(request)
-                .join();
+        try {
+            return BaseResponseBuilder.buildSuccessResponse(
+                    request.getRequestId(), requestPipeline.send(request).join());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return BaseResponseBuilder.buildErrorResponse(request.getRequestId(), ex);
+        }
     }
 
     @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    UserDetailResponse getUser(@PathVariable("id") String id) {
+    BaseResponse<UserDetailResponse> getUser(@PathVariable("id") String id) {
         GetUserRequest request = new GetUserRequest(id);
-        return requestPipeline.send(request)
-                .join();
+        try {
+            return BaseResponseBuilder.buildSuccessResponse(
+                    request.getRequestId(),
+                    requestPipeline.send(request).join());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return BaseResponseBuilder.buildErrorResponse(request.getRequestId(), ex);
+        }
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    UserResponse createUser(@RequestBody CreateUserRequest request) {
-        return requestPipeline.send(request)
-                .join();
+    BaseResponse<UserResponse> createUser(@RequestBody CreateUserRequest request) {
+        try {
+            return BaseResponseBuilder.buildSuccessResponse(
+                    request.getRequestId(),
+                    requestPipeline.send(request).join());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return BaseResponseBuilder.buildErrorResponse(request.getRequestId(), ex);
+        }
     }
 
     @PutMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    UserResponse updateUser(@PathVariable("id") String id, @RequestBody UpdateUserRequest request) {
+    BaseResponse<UserResponse> updateUser(@PathVariable("id") String id, @RequestBody UpdateUserRequest request) {
         request.setUserId(id);
-        return requestPipeline
-                .send(request)
-                .join();
+        try {
+            return BaseResponseBuilder.buildSuccessResponse(
+                    request.getRequestId(),
+                    requestPipeline.send(request).join());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return BaseResponseBuilder.buildErrorResponse(request.getRequestId(), ex);
+        }
     }
 
     @GetMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
-    UserDetailResponse userDetailResponse(@RequestBody LoginRequest request) {
-        return requestPipeline.send(request)
-                .join();
+    BaseResponse<UserDetailResponse> userDetailResponse(@RequestBody LoginRequest request) {
+        try {
+            return BaseResponseBuilder.buildSuccessResponse(
+                    request.getRequestId(),
+                    requestPipeline.send(request).join());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return BaseResponseBuilder.buildErrorResponse(request.getRequestId(), ex);
+        }
     }
 
 }
